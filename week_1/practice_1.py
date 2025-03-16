df0f1ab2e6652bf1aa2ac096080c20691888e06a
Maryam Awari <you@example.com>
2025-03-16 03:16:35 -0700

    (practice 1)

---
+++ week_1/practice_1.py
@@ -0,0 +1,14 @@
+# integer number
+num = 100
+print(num)
+print("Data type of variable number is", type(num))
+
+# float number
+fnum = 34.45
+print(fnum)
+print("Data type of variable float number is", type(fnum))
+
+# complex number
+cnum = 3 + 4j
+print(cnum)
+print("Data type of variable complex number is", type(cnum))
