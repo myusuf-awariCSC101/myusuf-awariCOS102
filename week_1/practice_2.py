d2c3e1fd064ee802470fbd34b520c7403caec96c
Maryam Awari <you@example.com>
2025-03-16 03:35:20 -0700

    (practice 2)

---
+++ week_1/practice_2.py
@@ -0,0 +1,11 @@
+# python program to print string and type
+str1 = "Hi My Name is Matthew. I am a String."
+str2 = "Hi My Name is Precious. I am also a String."
+
+# displaying string 1 and its data type
+print(str1)
+print(type(str1))
+
+# displaying string 2 and its data type
+print(str2)
+print(type(str2))
