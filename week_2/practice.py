20b7f66f5e45bfae573ae5f3ba9af0cdb72c1b4c
Maryam Awari <you@example.com>
2025-03-25 14:02:43 -0700

    (practice 1)

---
+++ week_2/practice.py
@@ -0,0 +1,21 @@
+x = 5 + 3 #addition
+print(x)
+
+y = 10 - 4 #subtraction
+print(y)
+
+z = 2 * 6 #multiplication
+print(z)
+
+a = 2**6
+print(a) #raised to the power
+
+b = 10 % 3 #this is  to find modulus
+print(b)
+
+c = abs(5-10) #this gives us the absolute value
+print(c)
+
+boy = "antimonopologeographicationalism"
+girl = boy[0:20:3]
+print(girl)
